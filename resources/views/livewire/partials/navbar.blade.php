70fdcf599f1427345d1091ff8d7edfd329f0e187
dikkycenter <dikkycenter@gmail.com>
2026-01-23 13:24:18 +0700

    update logo

--- resources/views/livewire/partials/navbar.blade.php
+++ resources/views/livewire/partials/navbar.blade.php
@@ -10,7 +10,7 @@ function navActive($route) {
 <nav class="bg-neutral-primary/20 backdrop-blur sticky w-full z-20 top-0 start-0 border-b border-default">
   <div class="max-w-7xl flex flex-wrap items-center justify-between mx-auto p-4">
     <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
-        <img src="{{ asset('storage/sepukul-logo.png')}}" class="h-7" alt="Sepukul Logo">
+        <img src="{{ asset('logo/sepukul-logo.png')}}" class="h-7" alt="Sepukul Logo">
         {{-- <span class="self-center text-xl text-heading font-semibold whitespace-nowrap">Sepukul</span> --}}
     </a>
     <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
