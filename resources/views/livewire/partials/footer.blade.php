1da399d9103fc481099b663e6cc7293e5eb85a82
dikkycenter <42606798+dikkycenter@users.noreply.github.com>
2026-02-03 15:44:04 +0700

    add mockup instagram

--- resources/views/livewire/partials/footer.blade.php
+++ resources/views/livewire/partials/footer.blade.php
@@ -5,56 +5,88 @@
         <div class="md:flex md:justify-between">
           <div class="mb-6 md:mb-0">
               <a href="/" class="flex items-center">
-                  <img src="{{ asset('logo/footer-logo.png')}}" class="h-10 me-3" alt="FlowBite Logo" />
+                  <img src="{{ asset('logo/footer-logo.png')}}" class="h-10 me-3" alt="Sepukul Logo" />
                   <span class="text-heading self-center text-2xl font-semibold whitespace-nowrap">Sepukul Padel Club</span>
               </a>
           </div>
-          <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-2">
-              <!-- <div>
-                  <h2 class="mb-6 text-sm font-semibold text-heading uppercase">Resources</h2>
-                  <ul class="text-body font-medium">
-                      <li class="mb-4">
-                          <a href="https://flowbite.com/" class="hover:underline">Flowbite</a>
-                      </li>
-                      <li>
-                          <a href="https://tailwindcss.com/" class="hover:underline">Tailwind CSS</a>
-                      </li>
-                  </ul>
-              </div> -->
+          <div class="grid grid-cols-2 gap-8 sm:gap-18 sm:grid-cols-3">
               <div>
-                  <h2 class="mb-6 text-sm font-semibold text-heading uppercase">Follow us</h2>
+                  <h2 class="mb-6 text-sm font-semibold text-heading uppercase">Main Menu</h2>
                   <ul class="text-body font-medium">
                       <li class="mb-4">
-                          <a href="https://github.com/themesberg/flowbite" class="hover:underline ">Instagram</a>
+                          <a href="{{ route('home') }}" class="hover:underline">Home</a>
+                      </li>
+                      <li class="mb-4">
+                          <a href="{{ route('leaderboard') }}" class="hover:underline">Leaderboard</a>
+                      </li>
+                      <li class="mb-4">
+                          <a href="#" class="hover:underline">Galery</a>
                       </li>
                       <li>
-                          <a href="https://discord.gg/4eeurUVvTy" class="hover:underline">Facebook</a>
+                          <a href="#" class="hover:underline">Contact</a>
                       </li>
                   </ul>
               </div>
               <div>
-                  <h2 class="mb-6 text-sm font-semibold text-heading uppercase">Legal</h2>
+                  <h2 class="mb-6 text-sm font-semibold text-heading uppercase">Follow Us</h2>
                   <ul class="text-body font-medium">
                       <li class="mb-4">
-                          <a href="#" class="hover:underline">Privacy Policy</a>
+                            <a 
+                                href="https://www.instagram.com/sepukulpadelclub/"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                x-on:click.prevent="
+                                    if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
+                                        window.location.href = 'instagram://user?username=sepukulpadelclub';
+                                        setTimeout(() => {
+                                            window.location.href = 'https://www.instagram.com/sepukulpadelclub/';
+                                        }, 800);
+                                    } else {
+                                        window.open('https://www.instagram.com/sepukulpadelclub/', '_blank');
+                                    }
+                                "
+                                class="hover:underline ">Instagram
+                            </a>
                       </li>
-                      <li>
+                  </ul>
+              </div>
+              <div>
+                  <h2 class="mb-6 text-sm font-semibold text-heading uppercase">Booking</h2>
+                  <ul class="text-body font-medium">
+                      <li class="mb-4">
+                          <a href="https://reclub.co/id/clubs/@sepukulpadel" target="_blank" class="hover:underline">Reclub</a>
+                      </li>
+                      <!-- <li>
                           <a href="#" class="hover:underline">Terms &amp; Conditions</a>
-                      </li>
+                      </li> -->
                   </ul>
               </div>
           </div>
       </div>
       <hr class="my-6 border-default sm:mx-auto lg:my-8" />
       <div class="sm:flex sm:items-center sm:justify-between">
-          <span class="text-sm text-body sm:text-center">© 2025 <a href="https://flowbite.com/" class="hover:underline">Sepukul™</a>. All Rights Reserved.
+          <span class="text-sm text-body sm:text-center">© 2025 <a href="/" class="hover:underline">Sepukul™</a>. All Rights Reserved.
           </span>
           <div class="flex mt-4 sm:justify-center sm:mt-0">
-            <a href="#" class="text-body hover:text-heading">
-                <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24"><path fill-rule="evenodd" d="M13.135 6H15V3h-1.865a4.147 4.147 0 0 0-4.142 4.142V9H7v3h2v9.938h3V12h2.021l.592-3H12V6.591A.6.6 0 0 1 12.592 6h.543Z" clip-rule="evenodd"/></svg>
-                <span class="sr-only">Facebook page</span>
+            <a
+                href="https://www.instagram.com/sepukulpadelclub/"
+                target="_blank"
+                rel="noopener noreferrer"
+                x-on:click.prevent="
+                    if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
+                        window.location.href = 'instagram://user?username=sepukulpadelclub';
+                        setTimeout(() => {
+                            window.location.href = 'https://www.instagram.com/sepukulpadelclub/';
+                        }, 800);
+                    } else {
+                        window.open('https://www.instagram.com/sepukulpadelclub/', '_blank');
+                    }
+                "
+                class="text-body hover:text-heading">
+                <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path fill-rule="evenodd" d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z" clip-rule="evenodd" /></svg>
+                <span class="sr-only">Instagram Page</span>
             </a>
-            <a href="#" class="text-body hover:text-heading ms-5">
+            <!-- <a href="#" class="text-body hover:text-heading ms-5">
                 <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24"><path d="M18.942 5.556a16.3 16.3 0 0 0-4.126-1.3 12.04 12.04 0 0 0-.529 1.1 15.175 15.175 0 0 0-4.573 0 11.586 11.586 0 0 0-.535-1.1 16.274 16.274 0 0 0-4.129 1.3 17.392 17.392 0 0 0-2.868 11.662 15.785 15.785 0 0 0 4.963 2.521c.41-.564.773-1.16 1.084-1.785a10.638 10.638 0 0 1-1.706-.83c.143-.106.283-.217.418-.331a11.664 11.664 0 0 0 10.118 0c.137.114.277.225.418.331-.544.328-1.116.606-1.71.832a12.58 12.58 0 0 0 1.084 1.785 16.46 16.46 0 0 0 5.064-2.595 17.286 17.286 0 0 0-2.973-11.59ZM8.678 14.813a1.94 1.94 0 0 1-1.8-2.045 1.93 1.93 0 0 1 1.8-2.047 1.918 1.918 0 0 1 1.8 2.047 1.929 1.929 0 0 1-1.8 2.045Zm6.644 0a1.94 1.94 0 0 1-1.8-2.045 1.93 1.93 0 0 1 1.8-2.047 1.919 1.919 0 0 1 1.8 2.047 1.93 1.93 0 0 1-1.8 2.045Z"/></svg>
                 <span class="sr-only">Discord community</span>
             </a>
@@ -69,7 +101,7 @@
             <a href="#" class="text-body hover:text-heading ms-5">
                 <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24"><path fill-rule="evenodd" d="M12 2a10 10 0 1 0 10 10A10.009 10.009 0 0 0 12 2Zm6.613 4.614a8.523 8.523 0 0 1 1.93 5.32 20.093 20.093 0 0 0-5.949-.274c-.059-.149-.122-.292-.184-.441a23.879 23.879 0 0 0-.566-1.239 11.41 11.41 0 0 0 4.769-3.366ZM10 3.707a8.82 8.82 0 0 1 2-.238 8.5 8.5 0 0 1 5.664 2.152 9.608 9.608 0 0 1-4.476 3.087A45.755 45.755 0 0 0 10 3.707Zm-6.358 6.555a8.57 8.57 0 0 1 4.73-5.981 53.99 53.99 0 0 1 3.168 4.941 32.078 32.078 0 0 1-7.9 1.04h.002Zm2.01 7.46a8.51 8.51 0 0 1-2.2-5.707v-.262a31.641 31.641 0 0 0 8.777-1.219c.243.477.477.964.692 1.449-.114.032-.227.067-.336.1a13.569 13.569 0 0 0-6.942 5.636l.009.003ZM12 20.556a8.508 8.508 0 0 1-5.243-1.8 11.717 11.717 0 0 1 6.7-5.332.509.509 0 0 1 .055-.02 35.65 35.65 0 0 1 1.819 6.476 8.476 8.476 0 0 1-3.331.676Zm4.772-1.462A37.232 37.232 0 0 0 15.113 13a12.513 12.513 0 0 1 5.321.364 8.56 8.56 0 0 1-3.66 5.73h-.002Z" clip-rule="evenodd"/></svg>
                 <span class="sr-only">Dribbble account</span>
-            </a>
+            </a> -->
           </div>
       </div>
     </div>
